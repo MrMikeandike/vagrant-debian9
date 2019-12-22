9e0685dc3a2aed8cd96ad521c68d14fc343061f1
Mikeandike <Mikeplusikes@gmail.com>
2019-12-21 18:16:17 -0600

    added things to init script to try and fix ansible not downloading roles correctly reliably. Added zsh install

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 # script that installs ansible
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367 \
-&& sudo apt update \
-&& sudo apt install ansible
+# ----------------------------
+# prereqs
+# - git: needed for galaxy
+# - dirmngr: needed to add apt-key
+# - python & python-pip
+# - python3 & python3-pip
+# - ca-certificates
+#----
+# - update ca certificates to try and avoid ssl issues with galaxy
+sudo apt update -y \
+&& sudo apt install --upgrade -y git dirmngr software-properties-common python python-pip python3 python3-pip ca-certificates \
+&& sudo update-ca-certificates
+# ---------------------------
+# install ansible
+sudo add-apt-repository 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' \
+&& sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367 \
+&& sudo apt update -y \
+&& sudo apt install -y ansible
